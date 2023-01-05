6e17b6a18fc2ab1a19b277223f3816d31615ca90
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-06 01:16:55 +0900

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("16進数を入力してください");
         int a = new java.util.Scanner(System.in).nextInt();
+        System.out.println(a);//警告を消すため
     }
 }
