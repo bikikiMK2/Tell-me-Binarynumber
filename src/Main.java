72953cf8dbfe4ccdf3f53a58a09e0b3217fa5cee
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-11 05:33:33 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("10進数を入力してください");
         int number = new java.util.Scanner(System.in).nextInt();
 
-        if(number < 1){
-            System.out.println(number);
-        }
-        else
-        {
-            int number2;
-            // あまりを入れておく変数
-            int number3 = 0;
-            //numberを2で割ったときの数値
+        int number2;
+        // あまりを入れておく変数
+        int number3;
+        //numberを2で割ったときの数値
 
-            List<Integer> LIST = new ArrayList<>();
+        List<Integer> LIST = new ArrayList<>();
 
-            while(number3 == 0){
+        while (number < 1) {
 
-                number2 = number % 2;
-                //numberを2で割ったときのあまり
-                LIST.add(number2);
-                number3 = number - number2;
-                number = number3;
+            number2 = number % 2;
+            //numberを2で割ったときのあまり
+            LIST.add(number2);
+            number3 = number - number2;
+            number = number3;
 
-                if(number3 < 1){
-                    LIST.add(number3);
-                }
-
+            if (number3 < 1) {
+                LIST.add(number3);
             }
+
         }
-        System.out.println();
-         //BinarynumbersListをconsoleに出力
+        //BinarynumbersListをconsoleに出力
+      }
     }
-}
