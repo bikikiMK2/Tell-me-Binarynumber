b860166f1f3ab2b17bf2df8d26892647b3da4baf
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-11 04:45:10 +0900

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args){
         System.out.println("16進数を入力してください");
-        int a = new java.util.Scanner(System.in).nextInt();
-        System.out.println(a);//警告を消すため
+        int number = new java.util.Scanner(System.in).nextInt();
+
+        if(number < 1){
+            System.out.println(number);
+        }
+        else{
+            int number2 = (0);
+            // 割ったあまり
+
+            List<Integer> Binarynumbers = new ArrayList<>();
+
+            while(number2 < 1){
+
+                number2 = number % 2;
+                int number3 = number - 2;
+
+                if(number3 < 1){
+                    Binarynumbers.add(number3);
+                }
+
+            }
+        }System.out.println();
+         //BinarynumbersListをconsoleに出力
+
     }
 }
