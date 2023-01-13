393be55a908f765e9dc847bbec4ff1424991ea81
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-13 23:22:27 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main{
     public static void main(String[] args) {
 
         System.out.println("10進数を入力してください");
         int number = new java.util.Scanner(System.in).nextInt();
 
-        System.out.println(BinarynumberList);
+        List<Integer>LIST =new ArrayList<>();
+
+        switch (number) {
+            case 0, 1 -> LIST.add(number);
+        }
+
+        System.out.println(LIST);
         //BinarynumbersListをconsoleに出力
       }
     }
