8a91cc3cd0ce5f13bdd1dffce2956df744cf1c8a
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-15 18:30:07 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,16 +12,16 @@ public class Main{
         else{
             while(number - (number%2) != 1){//numberが2になるまで
 
-                if((number - 1) % 2 == 0){//もしnumberが奇数なら
+                if((number - (number%2)) % 2 == 0){//もしnumberが奇数なら
                         LIST.add(number%2);
-                        number = (number - number%2);
+                        number = (number - number%2);//奇数を偶数化し下のifへ渡す
                 }//奇数の処理
 
-                if(number%2 == 0){//もしnumberが偶数なら
+                if((number - (number%2)) % 2 == 1){//もしnumberが偶数なら
                     while(number/2 == 1) {//numberが2になったら
-                        LIST.add(number/2);
+                        LIST.add(number/2);//0を追加
                         if((number/2)%2 == 1){//14などの数値偶数にしてループに帰してやる
-                            number = number/2;//奇数を追加
+                            number = number/2;//奇数を代入
                             number = (number - (number%2));
                         }
                         number = number/2;
