037ab1a5381cad09f42c6eb8104ee4f6e42a879a
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-15 18:11:39 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,25 @@ public class Main{
         int number = new java.util.Scanner(System.in).nextInt();
 
         List<Integer> LIST = new ArrayList<>();
-        if(number==1||number==0){
-            LIST.add(number);
-        }
+        if(number == 1||number == 0){LIST.add(number);}
         else{
             while(number - (number%2) != 1){//numberが2になるまで
 
+                if((number - 1) % 2 == 0){//もしnumberが奇数なら
+                        LIST.add(number%2);
+                        number = (number - number%2);
+                }//奇数の処理
+
                 if(number%2 == 0){//もしnumberが偶数なら
                     while(number/2 == 1) {//numberが2になったら
-                        LIST.add(number % 2);//0が追加
+                        LIST.add(number/2);
                         if((number/2)%2 == 1){//14などの数値偶数にしてループに帰してやる
-                            LIST.add(number%2);//0が追加
-                            number = number%2;//奇数が追加
+                            number = number/2;//奇数を追加
                             number = (number - (number%2));
                         }
+                        number = number/2;
+                        LIST.add(number%2);
+                        number = (number - (number%2));
                     }
                     if(number == 2){
                         number = number / 2;
@@ -29,18 +34,8 @@ public class Main{
                         break;
                     }
                 }//偶数を奇数になるまで2で余りを出す
-
-                if((number - 1) % 2 == 0){//もしnumberが奇数なら
-                    while(number / 2 == 1){//numberが2になるまで
-                        LIST.add(number%2);
-                        number = (number - (number - number%2));
-                    }
-                }//奇数の処理
             }
         }
-        if (number == 1) {
-            LIST.add(number);
-        }
-        System.out.println((LIST)+"2");
+        if (number == 1) {LIST.add(number);} System.out.println((LIST)+"2");
     }
 }
