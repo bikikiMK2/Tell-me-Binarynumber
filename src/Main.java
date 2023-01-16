3573a0844f723e69d6e82c18fe27663947dee97f
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-16 23:14:44 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,40 +11,21 @@ public class Main{
 
         if(number == 1||number == 0){LIST.add(number);}
         else{
+            while(number - (number%2) == 1){//2になるまで
 
-            //ここに後でif文を入れる
-
-            while(number - (number%2) != 1){
-
-                if((number - (number%2)) % 2 == 0){//もしnumberが奇数なら
-                        LIST.add(number%2);
-                        number = (number - number%2);//奇数を偶数化し下のifへ渡す
-                }//奇数の処理
-
-                if((number - (number%2)) % 2 == 1){//もしnumberが偶数なら
-                    while(number/2 == 1) {//numberが2になったら
-                        LIST.add(number/2);//0を追加
-                        if((number/2)%2 == 1){//14などの数値偶数にしてループに帰してやる
-                            number = number/2;//奇数を代入
-                            number = (number - (number%2));
-                        }
-                        if((number - (number%2)) % 2 == 0){//もしnumberが奇数なら
-                            LIST.add(number%2);
-                            number = (number - number%2);
-                        }//奇数の処理
-                        number = number/2;
-                        LIST.add(number%2);
-                        number = (number - (number%2));
+                    if( (number/2) % 2 == 1) {
+                        LIST.add(number % 2);
+                        number = (number - number / 2);
                     }
-                    if(number == 2){
-                        number = number / 2;
-                        LIST.add(number);
-                        break;
-                    }
-                }
+
+                        LIST.add(number % 2);
+                        number = (number - number % 2);
+
             }
+
+            LIST.add(number);
+
         }
-        if(number == 1) {LIST.add(number);}
         System.out.println((LIST)+"2");
     }
 }
