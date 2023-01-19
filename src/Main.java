bb370d6ad260590838c76cb564862923ec3bd736
Kawasaki Hibiki <86777445+bikikiMK2@users.noreply.github.com>
2023-01-20 00:17:51 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main{
         if(number == 1||number == 0){LIST.add(number);}
         else{
             while(number - (number%2) == 2){
-
+                
                     if( ((number/2) % 2 == 1) ){
                         LIST.add(number % 2);
                         number = (number - number / 2);
@@ -20,7 +20,6 @@ public class Main{
 
                     LIST.add(number % 2);
                     number = (number - number % 2);
-
             }
 
             LIST.add(number);
