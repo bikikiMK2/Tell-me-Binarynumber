096d2ad90c0ea38e6480e0bc7998e69dbcebd836
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-01-25 06:51:50 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,10 @@ public class Main{
 
         List<Integer> LIST = new ArrayList<>();
 
-        if(number == 1||number == 0){LIST.add(number);}
-        else{
-            while(number - (number%2) == 2){
-
-                    if( ((number/2) % 2 == 1) ){
-                        LIST.add(number % 2);
-                        number = (number - number / 2);
-                    }
-
-                    LIST.add(number % 2);
-                    number = (number - number % 2);
-
-            }
-
+        if(number == 1||number == 0) {
             LIST.add(number);
-
         }
+
         System.out.println(LIST);
     }
 }
