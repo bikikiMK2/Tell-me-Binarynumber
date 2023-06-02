a43d192ac113b3f8fc80dd915651269e000a8e4a
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-06-03 07:40:49 +0900

    Update calcBinarynumber.scala

---
+++ src/calcBinarynumber.scala
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+object calcBinarynumber extends App {
+  printf("10進数を入力してください")
+  val something:Double = new Scanner(System.in).nextDouble()
+  if(something == 0 || something == 1){
+    println(something)
+  }
+  else{
+    val k:Int = 0
+
+  }
+}
